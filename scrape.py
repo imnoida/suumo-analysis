269a93362544c96bd81c687e1c44151b859081fe
imnoida <82698327+imnoida@users.noreply.github.com>
2023-10-25 15:13:49 +0900

    update: ruffを導入してコーディング規約に従う

--- scrape.py
+++ scrape.py
@@ -3,8 +3,8 @@
 """
 from concurrent.futures import ThreadPoolExecutor
 
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 """
 ar:   area 北海道=010 東北=020 関東=030 甲信越・北陸=040 東海=050 関西=060 四国=070 中国=080 九州・沖縄=090
@@ -17,7 +17,6 @@ target_url = (
     "https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=050&bs=040&ta=23&pc=50&page={}"
 )
 
-url_list = []
 url_html = []
 
 
@@ -31,8 +30,7 @@ def parse_html(url):
     :return: HTML解析用のオブジェクト
     """
     response = request_html(url)
-    soup = BeautifulSoup(response.text, "lxml")
-    return soup
+    return BeautifulSoup(response.text, "lxml")
 
 
 def extract_max_page_number():
@@ -45,15 +43,14 @@ def extract_max_page_number():
     return int(max_page)
 
 
-def define_url_list(page_number):
-    for page in range(1, page_number + 1):
-        url_list.append(target_url.format(page))
+def define_url_list(page_number: int):
+    return [target_url.format(page) for page in range(1, page_number + 1)]
 
 
 def request_multiple_html(page_number):
-    define_url_list(page_number)
+    url_list = define_url_list(page_number)
     with ThreadPoolExecutor() as executor:
-        url_html = list(executor.map(request_html, url_list))
+        return list(executor.map(request_html, url_list))
 
 
 request_multiple_html(10)
