5da5572b2cc7ef329634be47a469a0e68684853c
imnoida <82698327+imnoida@users.noreply.github.com>
2023-11-07 16:35:06 +0900

    update: 沿線を抽出する

--- clean_data.py
+++ clean_data.py
@@ -21,5 +21,6 @@ df["礼金"] = df["礼金"].apply(extract_number)
 df["面積"] = df["面積"].apply(extract_number)
 df["築年数"] = df["築年数"].apply(extract_number)
 df = df.dropna(subset=["アクセス"])
+df["沿線"] = df["アクセス"].apply(lambda x: x.split("/")[0])
 
 df.plot()
