e84613e4de1f91a839737ea01b7e7691a931909e
imnoida <82698327+imnoida@users.noreply.github.com>
2023-11-07 16:38:00 +0900

    update: 市と群を抽出する

--- clean_data.py
+++ clean_data.py
@@ -13,6 +13,13 @@ def extract_number(value: str) -> float:
     return float(n[0])
 
 
+def extract_city(value: str) -> str:
+    n = re.findall(r"愛知県(.*市|.*郡)", value)
+    if len(n) == 0:
+        return "NaN"
+    return n[0]
+
+
 df["家賃"] = df["家賃"].apply(extract_number)
 df["管理費"] = df["管理費"].apply(extract_number)
 df["管理費"] /= 10000
@@ -26,5 +33,6 @@ df = df.query("沿線.str.contains('線') & ~沿線.str.contains('バス')")
 df["駅"] = df["アクセス"].apply(lambda x: x.split(" ")[0].split("/")[1])
 df = df.query("アクセス.str.contains('歩') & ~アクセス.str.contains('バス')")
 df["徒歩"] = df["アクセス"].apply(lambda x: int(re.findall(r"\d+", x.split(" ")[1])[0]))
+df["市"] = df["アドレス"].apply(extract_city)
 
 df.plot()
