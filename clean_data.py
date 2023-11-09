2e1ac1660e16e40ae6c865c78a6af9dd2bc17006
imnoida <82698327+imnoida@users.noreply.github.com>
2023-11-09 14:27:01 +0900

    Clean data (#31)
    * add: データの整形をする
    
    * update: アクセスがNullの行を削除
    
    * update: 沿線を抽出する
    
    * update: バスなどを排除
    
    * update: アクセスから駅を抽出
    
    * update: 徒歩圏内の物件を抽出する
    
    * update: 徒歩を抽出する
    
    * update: 市と群を抽出する
    
    * update: 区を抽出する

---
+++ clean_data.py
@@ -0,0 +1,47 @@
+import re
+
+import pandas as pd
+from pandas import DataFrame
+
+df: DataFrame = pd.read_csv("data/scrape.csv")
+
+
+def extract_number(value: str) -> float:
+    n = re.findall(r"[\d.]+", value)
+    if len(n) == 0:
+        return 0
+    return float(n[0])
+
+
+def extract_city(value: str) -> str:
+    n = re.findall(r"愛知県(.*市|.*郡)", value)
+    if len(n) == 0:
+        return "NaN"
+    return n[0]
+
+
+def extract_ward(value: str) -> str:
+    n = re.findall(r"愛知県名古屋市(.*区)", value)
+    if len(n) == 0:
+        return "NaN"
+    return n[0]
+
+
+df["家賃"] = df["家賃"].apply(extract_number)
+df["管理費"] = df["管理費"].apply(extract_number)
+df["管理費"] /= 10000
+df["敷金"] = df["敷金"].apply(extract_number)
+df["礼金"] = df["礼金"].apply(extract_number)
+df["面積"] = df["面積"].apply(extract_number)
+df["築年数"] = df["築年数"].apply(extract_number)
+df = df.dropna(subset=["アクセス"])
+df["沿線"] = df["アクセス"].apply(lambda x: x.split("/")[0])
+df = df.query("沿線.str.contains('線') & ~沿線.str.contains('バス')")
+df["駅"] = df["アクセス"].apply(lambda x: x.split(" ")[0].split("/")[1])
+df = df.query("アクセス.str.contains('歩') & ~アクセス.str.contains('バス')")
+df["徒歩"] = df["アクセス"].apply(lambda x: int(re.findall(r"\d+", x.split(" ")[1])[0]))
+df["市"] = df["アドレス"].apply(extract_city)
+df["区"] = df["アドレス"].apply(extract_ward)
+
+
+df.plot()
