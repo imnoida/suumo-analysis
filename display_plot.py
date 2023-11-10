76e802567feb608f4f2599482a4c27e44da322a0
imnoida <82698327+imnoida@users.noreply.github.com>
2023-11-10 14:49:30 +0900

    update: グラフの描画の調整をする

--- display_plot.py
+++ display_plot.py
@@ -1,14 +1,17 @@
 import matplotlib.pyplot as plt
+
 import clean_data
 
-cd = clean_data.clean_data().head(100)
+cd = clean_data.clean_data().head(10000)
 x = cd["市"]
 y = cd["家賃"]
-font = "MS Gothic"
-plt.xticks(fontname=font)
-plt.yticks(fontname=font)
-plt.xlabel("市", fontname=font)
-plt.ylabel("平均家賃", fontname=font)
+plt.rcParams["font.family"] = "MS Gothic"
+plt.figure(figsize=(15, 30))
+plt.title("市ごとの平均家賃")
+plt.xticks()
+plt.yticks()
+plt.xlabel("平均家賃")
+plt.ylabel("市")
 plt.grid()
-plt.bar(x, y)
+plt.barh(x, y)
 plt.show()
