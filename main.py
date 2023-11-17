64cb8658e59474c42a3bc8c8eee3c7cc15ea169c
imnoida <82698327+imnoida@users.noreply.github.com>
2023-11-17 13:24:01 +0900

    feat: フィルター機能を追加する

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ import display_plot
 
 if __name__ == "__main__":
     cd = clean_data.clean_data()
-    display_plot.display_plot(cd)
+    filtered = cd[
+        (cd["カテゴリー"] == "賃貸アパート")
+        & (cd["築年数"] < 10)
+        & (cd["家賃"] < 10)
+    ]
+    display_plot.display_plot(filtered)
