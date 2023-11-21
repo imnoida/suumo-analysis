a0792d77534dd7a7e01f9b206516463d348de9ea
imnoida <82698327+imnoida@users.noreply.github.com>
2023-11-21 16:41:33 +0900

    feat: ヒストグラムを表示する

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import display_plot
 if __name__ == "__main__":
     cd = clean_data.clean_data()
     filtered = cd[
-        (cd["カテゴリー"] == "賃貸アパート")
-        & (cd["築年数"] < 10)
-        & (cd["家賃"] < 10)
+        (cd["家賃"] < 10)
     ]
-    display_plot.display_plot(filtered)
+    display_plot.average_rent_graph(filtered)
+    display_plot.rent_hist(filtered)
