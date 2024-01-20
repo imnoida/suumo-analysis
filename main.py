97624aee14c2e62d3899ec497a62808bc375e3bf
imnoida <82698327+imnoida@users.noreply.github.com>
2024-01-20 15:00:15 +0900

    fix: 引数に入れる変数を変更

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import model
 
 if __name__ == "__main__":
     cd = clean_data.clean_data()
-    co = clean_outlier.clean_outlier(cd)
-    display_plot.average_rent_graph(cd)
-    filtered = co
+    filtered = clean_outlier.clean_outlier(cd)
+    display_plot.average_rent_graph(filtered)
     display_plot.rent_hist(filtered)
     model.main()
