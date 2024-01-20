b9c338e3aec28ccb23ddd869344677fe5da006ec
imnoida <82698327+imnoida@users.noreply.github.com>
2024-01-20 17:15:59 +0900

    feat: 空白の画像が保存される現象を修正

--- display_plot.py
+++ display_plot.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 from pandas import DataFrame
+from pathlib import Path
 
+
+def save_png(file_name: str) -> None:
+    image_path = Path("image")
+    image_path.mkdir(exist_ok=True, parents=True)
+    plt.savefig(image_path / f"{file_name}.png")
 
 def average_rent_graph(cd: DataFrame) -> None:
     average_rent_per_city = cd.groupby("地域")["家賃"].mean()
@@ -15,8 +21,8 @@ def average_rent_graph(cd: DataFrame) -> None:
     plt.ylabel("地域")
     plt.grid()
     plt.barh(x, y)
+    save_png("average_rent_graph")
     plt.show()
-
 
 def rent_hist(cd: DataFrame) -> None:
     plt.rcParams["font.family"] = "MS Gothic"
@@ -28,4 +34,5 @@ def rent_hist(cd: DataFrame) -> None:
     plt.ylabel("件数")
     plt.grid()
     plt.hist(cd["家賃"], bins=50)
+    save_png("rent_hist")
     plt.show()
